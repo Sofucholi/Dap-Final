6a8c6418e421915bbbb6324532b9874fe3f3afdf
maicumaicu <agusprietovaldez@gmail.com>
2022-05-19 11:59:20 -0300

    agrego recyclerView y todo lo importante

---
+++ app/src/main/java/com/example/feli/feli.kt
@@ -0,0 +1,8 @@
+package com.example.feli
+
+import android.graphics.drawable.Drawable
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class feli(var facha:Int, var tipo:String, var habilidadesEspeciales:List<String>, var Profile: String):Parcelable
